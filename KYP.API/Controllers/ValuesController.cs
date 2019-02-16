3a075b8d1c681752f100d32621006cc23338e140
ViditKShah <viditkshah@gmail.com>
2019-02-16 13:31:11 -0500

    Parent to Child Component communication using Input properties

--- KYP.API/Controllers/ValuesController.cs
+++ KYP.API/Controllers/ValuesController.cs
@@ -22,6 +22,7 @@ namespace KYP.API.Controllers
         }
         
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
