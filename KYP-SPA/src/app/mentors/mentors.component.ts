cbb342afc7c7c0928849a0c313c060342e2843b9
ViditKShah <viditkshah@gmail.com>
2019-02-27 06:54:31 -0500

    Retrieving user data using service in Angular

--- KYP-SPA/src/app/mentors/mentors.component.ts
+++ KYP-SPA/src/app/mentors/mentors.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../_models/user';
+import { AlertifyService } from '../_services/alertify.service';
+import { UserService } from '../_services/user.service';
 
 @Component({
   selector: 'app-mentors',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mentors.component.css']
 })
 export class MentorsComponent implements OnInit {
+  users: User[];
 
-  constructor() { }
+  constructor(private userService: UserService, private alertifyService: AlertifyService) { }
 
   ngOnInit() {
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    this.userService.getUsers().subscribe((users: User[]) => {
+      this.users = users;
+    }, error => {
+      this.alertifyService.error(error);
+    });
   }
 
 }
