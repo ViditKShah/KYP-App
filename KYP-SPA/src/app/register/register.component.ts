0e4e7f2657162f30daae8d2315125ff35fce7bc9
ViditKShah <viditkshah@gmail.com>
2019-03-17 11:19:12 -0400

    Basic Conversion of template to reactive form

--- KYP-SPA/src/app/register/register.component.ts
+++ KYP-SPA/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -11,18 +12,26 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
 
   model: any = {};
+  registerForm: FormGroup;
 
   constructor(private authService: AuthService, private alertifyService: AlertifyService) { }
 
   ngOnInit() {
+    this.registerForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl()
+    });
   }
 
   register() {
-    this.authService.register(this.model).subscribe(() => {
-      this.alertifyService.success('Registration successful!');
-    }, error => {
-      this.alertifyService.error(error);
-    });
+    // this.authService.register(this.model).subscribe(() => {
+    //   this.alertifyService.success('Registration successful!');
+    // }, error => {
+    //   this.alertifyService.error(error);
+    // });
+
+    console.log(this.registerForm.value);
   }
 
   cancel() {
