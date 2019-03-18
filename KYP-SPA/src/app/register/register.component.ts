f1b96c54a083c05a60ec4c88dbd1af9abf1a8d93
ViditKShah <viditkshah@gmail.com>
2019-03-17 21:08:20 -0400

    Completing registration implementation in angular

--- KYP-SPA/src/app/register/register.component.ts
+++ KYP-SPA/src/app/register/register.component.ts
@@ -3,6 +3,8 @@ import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { BsDatepickerConfig } from 'ngx-bootstrap';
+import { User } from '../_models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -12,11 +14,13 @@ import { BsDatepickerConfig } from 'ngx-bootstrap';
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
 
-  model: any = {};
+  user: User;
   registerForm: FormGroup;
   bsConfig: Partial<BsDatepickerConfig>;
 
-  constructor(private authService: AuthService, private alertifyService: AlertifyService, private fb: FormBuilder) { }
+  constructor(private authService: AuthService,
+    private alertifyService: AlertifyService, private fb: FormBuilder,
+    private router: Router) { }
 
   ngOnInit() {
     this.bsConfig = {
@@ -44,13 +48,18 @@ export class RegisterComponent implements OnInit {
   }
 
   register() {
-    // this.authService.register(this.model).subscribe(() => {
-    //   this.alertifyService.success('Registration successful!');
-    // }, error => {
-    //   this.alertifyService.error(error);
-    // });
-
-    console.log(this.registerForm.value);
+    if (this.registerForm.valid) {
+      this.user = Object.assign({}, this.registerForm.value);
+      this.authService.register(this.user).subscribe(() => {
+        this.alertifyService.success('Registration successful!');
+      }, error => {
+        this.alertifyService.error(error);
+      }, () => {
+        this.authService.login(this.user).subscribe(() => {
+          this.router.navigate(['/mentors']);
+        });
+      });
+    }
   }
 
   cancel() {
