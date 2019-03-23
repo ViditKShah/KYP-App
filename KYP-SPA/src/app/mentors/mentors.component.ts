19c07c313be0bb58052ca5b6b4b7bdebf02e61e3
ViditKShah <viditkshah@gmail.com>
2019-03-23 14:01:09 -0400

    Using ngx-bootstrap pagination module for angular

--- KYP-SPA/src/app/mentors/mentors.component.ts
+++ KYP-SPA/src/app/mentors/mentors.component.ts
@@ -3,6 +3,7 @@ import { User } from '../_models/user';
 import { AlertifyService } from '../_services/alertify.service';
 import { UserService } from '../_services/user.service';
 import { ActivatedRoute } from '@angular/router';
+import { Pagination, PaginatedResult } from '../_models/pagination';
 
 @Component({
   selector: 'app-mentors',
@@ -11,13 +12,31 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MentorsComponent implements OnInit {
   users: User[];
+  pagination: Pagination;
 
   constructor(private userService: UserService, private alertifyService: AlertifyService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.data.subscribe((data) => {
       this.users = data['users'].result;
+      this.pagination = data['users'].pagination;
     });
+  }
+
+  pageChanged(event: any): void {
+    this.pagination.currentPage = event.page;
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    this.userService
+        .getUsers(this.pagination.currentPage, this.pagination.itemsPerPage)
+        .subscribe((res: PaginatedResult<User[]>) => {
+          this.users = res.result;
+          this.pagination = res.pagination;
+        }, error => {
+          this.alertifyService.error(error);
+        });
   }
 
 }
